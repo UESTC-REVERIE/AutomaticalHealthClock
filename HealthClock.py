2987af1d3da6e7433ffc57f8299a3fc3f74fa555
“REVERIE” <614158101@qq.com>
2022-08-03 17:32:15 +0800

    无法获取GPS信息，待解决

--- HealthClock.py
+++ HealthClock.py
@@ -1,1 +1,29 @@
+from ctypes import sizeof
+from tkinter import CENTER, LEFT, RIGHT
+from turtle import left, right, tilt
 import pyautogui
+import time
+import keyboard
+import sys
+def whereami():
+    print(pyautogui.position())
+if __name__=='__main__':
+    def clickBySprite(url,warning):
+        time_start=time.time()
+        while True:
+            time_end=time.time()
+            pos=pyautogui.locateOnScreen(url)
+            if pos!=None:
+                pyautogui.click(pos,button='left')
+                return
+            if time_end-time_start>=30:
+                print(warning)#所有print之后切换到记录本中用于查看
+                sys.exit()
+    clickBySprite('sprite/StartWeChat.png',"Wechat Not Open")
+    clickBySprite('sprite/Applets.png',"Network Error")
+    clickBySprite('sprite/UESTC.png',"Applet Not Find")
+    clickBySprite('sprite/Hub.png',"UESTC Not Open")
+    clickBySprite('sprite/target.png',"Target Not Find")
+    
+    #keyboard.add_hotkey('up',whereami)
+    #keyboard.wait('esc')
---
+++ test.bat
@@ -0,0 +1,7 @@
+C:
+cd C:/Program Files (x86)/Tencent/WeChat
+start WeChat.exe
+D:
+cd D:\Automatic
+python HealthClock.py
+pause
